129d474db63b5d4c7466b50dbfbd92d8bf8464cd
GM-Benji <67319509+GM-Benji@users.noreply.github.com>
2023-11-19 15:12:28 +0100

    Update generator.c

--- generator.c
+++ generator.c
@@ -7,7 +7,20 @@ element* generate(char board[][],int color)
 	{
 		for (int j = 0; j < 8; j++)
 		{
-
+			if ((board[i][j] == 'n'&& color==0) || (board[i][j] == 'N' && color==1))
+			{
+				for (int k = 0; k < 8; k++)
+				{
+					int z=int(board[digi(i * 10 + j + moves[0][k],0)][digi(i * 10 + j + moves[0][k],1)]);//rzutowanie pola na int(czyli kod ascii)
+					if (
+						(i * 10 + j + moves[0][k] >= 0 && i * 10 + j + moves[0][k] <= 77 )//sprawdzanie czy nie wychodzi poza szachownice
+						&&( (z<=90 &&color==0) || ((z>90 || z==35)&& color==1) ) sprawdzanie czy nie stoi figura tego samego koloru(z kodu ascii)
+						)
+					{
+						
+					}
+				}
+			}
 		}
 	}
 }
