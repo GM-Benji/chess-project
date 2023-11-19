d1ea9c6caffd82a117ccb4915a42f63981195549
GM-Benji <67319509+GM-Benji@users.noreply.github.com>
2023-11-19 14:55:26 +0100

    Create generator.c
    generuje ruchy dla danego koloru

---
+++ generator.c
@@ -0,0 +1,13 @@
+element* generate(char board[][],int color)
+{
+	int* moves[6][32];
+	init(moves);
+	element *head={.nastepny=NULL,.ruch={.poz1=0,.poz2=0}};
+	for (int i = 0; i < 8; i++)
+	{
+		for (int j = 0; j < 8; j++)
+		{
+
+		}
+	}
+}
