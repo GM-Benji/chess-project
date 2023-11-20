1d34cd0d1c43de6a68880429402bef1e4bca95d4
GM-Benji <67319509+GM-Benji@users.noreply.github.com>
2023-11-20 20:10:26 +0100

    Update generator.c

--- generator.c
+++ generator.c
@@ -1,8 +1,11 @@
-element* generate(char board[][],int color)
+element* generate(char board[8][8],int color)
 {
 	int* moves[6][32];
 	init(moves);
-	element *head={.nastepny=NULL,.ruch={.poz1=0,.poz2=0}};
+	move r={.pos1=0,.pos2=0};
+	element* head = malloc(sizeof(element));
+	*head = (element){ .ruch = r,.nastepny = NULL };
+	
 	for (int i = 0; i < 8; i++)
 	{
 		for (int j = 0; j < 8; j++)
@@ -11,16 +14,18 @@ element* generate(char board[][],int color)
 			{
 				for (int k = 0; k < 8; k++)
 				{
-					int z=int(board[digi(i * 10 + j + moves[0][k],0)][digi(i * 10 + j + moves[0][k],1)]);//rzutowanie pola na int(czyli kod ascii)
+					int z=(int)(board[digi(i * 10 + j + *moves[0][k],0)][digi(i * 10 + j + *moves[0][k],1)]);//rzutowanie pola na int(czyli kod ascii)
 					if (
-						(i * 10 + j + moves[0][k] >= 0 && i * 10 + j + moves[0][k] <= 77 )//sprawdzanie czy nie wychodzi poza szachownice
+						(i * 10 + j + *moves[0][k] >= 0 && i * 10 + j + *moves[0][k] <= 77 )//sprawdzanie czy nie wychodzi poza szachownice
 						&&( (z<=90 &&color==0) || ((z>90 || z==35)&& color==1) ) //sprawdzanie czy nie stoi figura tego samego koloru(z kodu ascii)
 						)
 					{
-						
+						move ruch={.pos1=i * 10 + j,.pos2=i * 10 + j + *moves[0][k]};
+						utworz(ruch,head);
 					}
 				}
 			}
 		}
 	}
+	return head;
 }
