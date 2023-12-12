32f9de0801a84d49745820b40487a70072edccbe
girenkol <82716636+girenkol@users.noreply.github.com>
2023-12-12 16:48:22 +0100

    Create a.c

---
+++ a.c
@@ -0,0 +1,292 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "game.c"
+
+typedef struct set {
+	char board[8][8];
+}set;
+set setInit(char board[8][8])
+{
+	set game;
+	for(int i=0;i<8;i++)
+	{
+		for(int j=0;j<8;j++)
+		{
+			game.board[i][j]=board[i][j];
+		}
+	}
+	return game;
+}
+
+element* generate(set game,int color)
+{
+	arrType moves=init();
+	move r={.pos1=0,.pos2=0};
+	element* head = malloc(sizeof(element));
+	*head = (element){ .ruch = r,.nastepny = NULL };
+	move ruch={.pos1=-1,.pos2=-1};
+	for (int i = 0; i < 8; i++)
+	{
+		for (int j = 0; j < 8; j++)
+		{
+			if ((game.board[i][j] == 'n'&& color==0) || (game.board[i][j] == 'N' && color==1))//skoczki
+			{
+				for (int k = 0; k < 8; k++)
+				{
+					ruch=(move){.pos1=i*10+j,.pos2=i*10+j+moves.arr[0][k]};
+					if(ifLegal(color,ruch,game.board))
+					{
+						utworz(ruch,head);
+					}
+				}
+			}
+			if ((game.board[i][j] == 'P' && color))//biale pionki
+			{
+				for (int k = 0; k < 4; k++)
+				{
+					ruch=(move){.pos1=i*10+j,.pos2=i*10+j+moves.arr[1][k]};
+					if(ifLegal(color,ruch,game.board))
+					{
+					    if(digi(ruch.pos1, 0) == 1)
+						{
+						    int temp = ruch.pos1;
+                            ruch.pos1 = ruch.pos2;
+                            utworz(ruch, head); //promocja na hetmana np. 4 do 4
+                            ruch.pos1 = temp;
+                            ruch.pos2 = temp;
+                            utworz(ruch, head); //promocja na skoczka np. 14 do 14
+							ruch.pos1 += 10;
+                            ruch.pos2 += 10;
+							utworz(ruch, head); //promocja na gonca np. 24 do 24
+							ruch.pos1 += 10;
+                            ruch.pos2 += 10;
+							utworz(ruch, head); //promocja na wieze 34 do 34
+						}
+						else utworz(ruch,head);
+					}
+				}
+			}
+			if ((game.board[i][j] == 'p' && !color))//czarne pionki
+			{
+				for (int k = 0; k < 4; k++)
+				{
+					ruch=(move){.pos1=i*10+j,.pos2=i*10+j+moves.arr[2][k]};
+					if(ifLegal(color,ruch,game.board))
+					{
+						if(digi(ruch.pos2, 0) == 7)
+						{
+						    int temp = ruch.pos1;
+                            ruch.pos1 = ruch.pos2;
+                            utworz(ruch, head); //promocja na hetmana np. 74 do 74
+                            ruch.pos1 = temp;
+                            ruch.pos2 = temp;
+                            utworz(ruch, head); //promocja na skoczka np. 64 do 64
+							ruch.pos1-=10;
+							ruch.pos2-=10;
+							utworz(ruch, head); //promocja na gonca np. 54 do 54
+							ruch.pos1-=10;
+							ruch.pos2-=10;
+							utworz(ruch, head); //promocja na wieze np. 44 do 44
+						}
+						else utworz(ruch,head);
+					}
+
+
+				}
+			}
+			if ((game.board[i][j] == 'r'&& color==0) || (game.board[i][j] == 'R' && color==1))//wieze
+			{
+				for (int k = 1; k <= 28; k++)
+				{
+
+					ruch=(move){.pos1=i*10+j,.pos2=i*10+j+moves.arr[3][k]};
+					if(digi(ruch.pos1,0)!=digi(ruch.pos1+digi(moves.arr[3][k],1),0))continue;
+					if(ifLegal(color,ruch,game.board))
+					{
+						utworz(ruch,head);
+					}
+				}
+			}
+			if ((game.board[i][j] == 'b' && color==0) || (game.board[i][j] == 'B' && color==1)) //gonce
+            {
+                for(int k=1; k < 32; k++)
+                {
+                    ruch=(move){.pos1=i*10+j,.pos2=i*10+j+moves.arr[4][k]};
+					if(abs(digi(ruch.pos1,0)-digi(ruch.pos2,0))!=abs(digi(ruch.pos1,1)-digi(ruch.pos2,1)))continue;
+                    if(ifLegal(color,ruch,game.board))
+					{
+						utworz(ruch,head);
+					}
+                }
+            }
+			if ((game.board[i][j] == 'q' && color==0) || (game.board[i][j] == 'Q' && color==1)) //hetmany
+			{
+				for (int k = 1; k <= 28; k++)
+				{
+
+					ruch=(move){.pos1=i*10+j,.pos2=i*10+j+moves.arr[3][k]};
+					if(digi(ruch.pos1,0)!=digi(ruch.pos1+digi(moves.arr[3][k],1),0))continue;
+					if(ifLegal(color,ruch,game.board))
+					{
+						utworz(ruch,head);
+					}
+				}
+				for(int k=0; k < 32; k++)
+                {
+                    ruch=(move){.pos1=i*10+j,.pos2=i*10+j+moves.arr[4][k]};
+					if(abs(digi(ruch.pos1,0)-digi(ruch.pos2,0))!=abs(digi(ruch.pos1,1)-digi(ruch.pos2,1)))continue;
+                    if(ifLegal(color,ruch,game.board))
+					{
+						utworz(ruch,head);
+					}
+                }
+			}
+			if ((game.board[i][j] == 'k' && color==0) || (game.board[i][j] == 'K' && color==1)) //krole
+            {
+                for(int k=0; k < 8; k++)
+                {
+                    ruch=(move){.pos1=i*10+j,.pos2=i*10+j+moves.arr[5][k]};
+                    if(ifLegal(color,ruch,game.board))
+					{
+						utworz(ruch,head);
+					}
+                }
+            }
+		}
+	}
+	return head;
+}
+
+char board[8][8] = {
+    { '#', '#', '#', '#', '#', '#', '#', 'B' },
+    { '#', '#', '#', '#', '#', '#', '#', '#' },
+    { '#', '#', '#', '#', '#', '#', 'K', '#' },
+    { '#', '#', '#', '#', '#', '#', '#', '#' },
+    { '#', '#', '#', 'R', '#', '#', '#', '#' },
+    { '#', '#', '#', '#', '#', '#', '#', '#' },
+    { '#', 'r', '#', '#', '#', '#', '#', 'R' },
+    { 'k', '#', '#', '#', '#', '#', '#', '#' }
+};
+
+int gameOver(char board[8][8]) //do zrobienia w innym pliku bo z tego beda korzystac inne funkcje
+{
+    return 0;
+}
+
+move typeMove() //trzeba jeszcze dorobiæ ¿eby pobiera³o info z funkcji sprawdzaj¹cej czy ruch jest mo¿liwy do wykonania
+{// dopisac zeby sprawdzalo czy wpisany ruch jest w liscie z tych wygenerowanych, wtedy if else z iflegalem niepotrzebny
+    char kolumny[8] = {'a','b','c','d','e','f','g','h'}; //tablice pomocnicze do porownania z inputem
+    char wiersze[8] = {'8', '7', '6', '5', '4', '3', '2', '1'};
+
+    move conv; //na to skonwertowane
+    conv.pos1 = NULL;
+    conv.pos2 = NULL;
+
+    printf("    it's your turn to make a move\n");
+
+    int isInputOk; //zmienna ktora pomoga sprawdzic czy input jest poprawnie wpisany
+    do
+    {
+        isInputOk = 1;
+        conv.pos1 = NULL;
+        conv.pos2 = NULL;
+        char takeMove[5];
+        printf("(np. g8-f6) >> ");
+        scanf("%s", takeMove);
+
+        int isOkey0=0, isOkey1=0, isOkey3=0, isOkey4=0;
+
+        for(int i=0; i<=8 && (isOkey0 == 0 || isOkey1 == 0 || isOkey3 == 0 || isOkey4 == 0); i++) //petla leci po calej kolumny i wiersze oraz jezeli wszystko zostalo przekonwertowane poprawnie to wszyskie isOkey zmieniaja sie na 1
+        {
+            if(isOkey0 == 0 && takeMove[0] == kolumny[i])
+            {
+                conv.pos1 += i;
+                isOkey0=1;
+            }
+            if(isOkey1 == 0 && takeMove[1] == wiersze[i])
+            {
+                conv.pos1 += i*10;
+                isOkey1=1;
+            }
+            if(isOkey3 == 0 && takeMove[3] == kolumny[i])
+            {
+                conv.pos2 += i;
+                isOkey3=1;
+            }
+            if(isOkey4 == 0 && takeMove[4] == wiersze[i])
+            {
+                conv.pos2 += i*10;
+                isOkey4=1;
+            }
+        }
+
+        if(isOkey0 == 0 || isOkey1 == 0 || isOkey3 == 0 || isOkey4 == 0) // jezeli cos nie zostalo zmienione to cos zostalo zle wpisane
+        {
+            printf("invalid input notation\n");
+            isInputOk = 0;
+        }
+        else // jezeli zostalo wpisane dobrze to wypisuje ze teraz sie dzieje analiza planszy, zmienia isInputOk i nie robi petli jeszcze raz
+        {
+            if(!(ifLegal(1,conv,board))) // tu musi byæ warunek ze jezeli ruch jest niemozliwy bo gracz jest szacha albo po drodze ruchu coœ stoi
+            {
+                printf("You cant make this move\n");
+                isInputOk = 0;
+            }
+            else
+            {
+                printf("pricessing the move..\n");
+                isInputOk = 1;
+            }
+        }
+
+        //printf("%d | %d\n", conv.pos1, conv.pos2);
+    } while(isInputOk == 0); // jezeli cos nie zostalo zmienione to powtarza wpisywanie
+    //printf("\n%d\n", isInputOk);
+
+    moveMaker(board, conv);
+
+    return conv;
+}
+
+void main()
+{
+	//char board[8][8] = { "rnbqkbnr","pppppppp","########","########","########","########","PPPPPPPP","RNBQKBNR"};
+	//char board[8][8] = { "########","######B#","########","K#######","########","##b#####","########","k#######"};
+	move x={.pos1=71,.pos2=51};
+	drawBoard(board);
+
+    //isCheck(board, 0);
+	//zniszcz(head);
+	printf("%d\n",ifLegal(0,x,board));
+    printf("%d\n",isCheck(board, 0));
+
+    set game = setInit(board);
+    drawBoard(game.board);
+
+    element *head = malloc(sizeof(element));
+	head = generate(game,0);
+
+	showL(head->nastepny);
+
+/*
+	for(int i=0; i<8; i++) //sprawdzacz - to jest niepotrzebne póŸniej
+    {
+        for(int j=0; j<8; j++)
+            printf(" %2d", i*10+j);
+        printf("\n");
+    }
+
+    while(gameOver(board)==0)
+    {
+        drawBoard(board);
+        move playersMove = typeMove();
+    }
+
+    if(gameOver(board) == 1) //win
+        printf("white wins");
+    else if(gameOver(board) == 2) //lose
+        printf("black wins");
+    else if(gameOver(board) == 3)
+        printf("pat / draw"); //draw
+*/
+}
