ffa2d95b5eaa87a307ba16d55a0690350c420112
GM-Benji <67319509+GM-Benji@users.noreply.github.com>
2023-12-12 16:37:38 +0100

    Update generator.c

--- generator.c
+++ generator.c
@@ -1,4 +1,4 @@
-element* generate(char board[8][8],int color)
+element* generate(set game,int color)
 {
 	arrType moves=init();
 	move r={.pos1=0,.pos2=0};
@@ -9,29 +9,29 @@ element* generate(char board[8][8],int color)
 	{
 		for (int j = 0; j < 8; j++)
 		{
-			if ((board[i][j] == 'n'&& color==0) || (board[i][j] == 'N' && color==1))//skoczki
+			if ((game.board[i][j] == 'n'&& color==0) || (game.board[i][j] == 'N' && color==1))//skoczki
 			{
 				for (int k = 0; k < 8; k++)
 				{
 					ruch=(move){.pos1=i*10+j,.pos2=i*10+j+moves.arr[0][k]};
-					if(ifLegal(color,ruch,board))
+					if(ifLegal(color,ruch,game.board))
 					{
 						utworz(ruch,head);
 					}
 				}
 			}
-			if ((board[i][j] == 'P' && color))//biale pionki
+			if ((game.board[i][j] == 'P' && color))//biale pionki
 			{
 				for (int k = 0; k < 4; k++)
 				{
 					ruch=(move){.pos1=i*10+j,.pos2=i*10+j+moves.arr[1][k]};
-					if(ifLegal(color,ruch,board))
+					if(ifLegal(color,ruch,game.board))
 					{
 					    if(digi(ruch.pos1, 0) == 1)
 						{
 						    int temp = ruch.pos1;
                             ruch.pos1 = ruch.pos2;
-                            utworz(ruch, head); //promocja na hetmana np. 4 do 74
+                            utworz(ruch, head); //promocja na hetmana np. 4 do 4
                             ruch.pos1 = temp;
                             ruch.pos2 = temp;
                             utworz(ruch, head); //promocja na skoczka np. 14 do 14
@@ -46,12 +46,12 @@ element* generate(char board[8][8],int color)
 					}
 				}
 			}
-			if ((board[i][j] == 'p' && !color))//czarne pionki
+			if ((game.board[i][j] == 'p' && !color))//czarne pionki
 			{
 				for (int k = 0; k < 4; k++)
 				{
 					ruch=(move){.pos1=i*10+j,.pos2=i*10+j+moves.arr[2][k]};
-					if(ifLegal(color,ruch,board))
+					if(ifLegal(color,ruch,game.board))
 					{
 						if(digi(ruch.pos2, 0) == 7)
 						{
@@ -74,39 +74,39 @@ element* generate(char board[8][8],int color)
 
 				}
 			}
-			if ((board[i][j] == 'r'&& color==0) || (board[i][j] == 'R' && color==1))//wieze
+			if ((game.board[i][j] == 'r'&& color==0) || (game.board[i][j] == 'R' && color==1))//wieze
 			{
 				for (int k = 1; k <= 28; k++)
 				{
 
 					ruch=(move){.pos1=i*10+j,.pos2=i*10+j+moves.arr[3][k]};
 					if(digi(ruch.pos1,0)!=digi(ruch.pos1+digi(moves.arr[3][k],1),0))continue;
-					if(ifLegal(color,ruch,board))
+					if(ifLegal(color,ruch,game.board))
 					{
 						utworz(ruch,head);
 					}
 				}
 			}
-			if ((board[i][j] == 'b' && color==0) || (board[i][j] == 'B' && color==1)) //gonce
+			if ((game.board[i][j] == 'b' && color==0) || (game.board[i][j] == 'B' && color==1)) //gonce
             {
                 for(int k=1; k < 32; k++)
                 {
                     ruch=(move){.pos1=i*10+j,.pos2=i*10+j+moves.arr[4][k]};
 					if(abs(digi(ruch.pos1,0)-digi(ruch.pos2,0))!=abs(digi(ruch.pos1,1)-digi(ruch.pos2,1)))continue;
-                    if(ifLegal(color,ruch,board))
+                    if(ifLegal(color,ruch,game.board))
 					{
 						utworz(ruch,head);
 					}
                 }
             }
-			if ((board[i][j] == 'q' && color==0) || (board[i][j] == 'Q' && color==1)) //hetmany
+			if ((game.board[i][j] == 'q' && color==0) || (game.board[i][j] == 'Q' && color==1)) //hetmany
 			{
 				for (int k = 1; k <= 28; k++)
 				{
 
 					ruch=(move){.pos1=i*10+j,.pos2=i*10+j+moves.arr[3][k]};
 					if(digi(ruch.pos1,0)!=digi(ruch.pos1+digi(moves.arr[3][k],1),0))continue;
-					if(ifLegal(color,ruch,board))
+					if(ifLegal(color,ruch,game.board))
 					{
 						utworz(ruch,head);
 					}
@@ -115,18 +115,18 @@ element* generate(char board[8][8],int color)
                 {
                     ruch=(move){.pos1=i*10+j,.pos2=i*10+j+moves.arr[4][k]};
 					if(abs(digi(ruch.pos1,0)-digi(ruch.pos2,0))!=abs(digi(ruch.pos1,1)-digi(ruch.pos2,1)))continue;
-                    if(ifLegal(color,ruch,board))
+                    if(ifLegal(color,ruch,game.board))
 					{
 						utworz(ruch,head);
 					}
                 }
 			}
-			if ((board[i][j] == 'k' && color==0) || (board[i][j] == 'K' && color==1)) //krole
+			if ((game.board[i][j] == 'k' && color==0) || (game.board[i][j] == 'K' && color==1)) //krole
             {
                 for(int k=0; k < 8; k++)
                 {
                     ruch=(move){.pos1=i*10+j,.pos2=i*10+j+moves.arr[5][k]};
-                    if(ifLegal(color,ruch,board))
+                    if(ifLegal(color,ruch,game.board))
 					{
 						utworz(ruch,head);
 					}
