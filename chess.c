9d0ded7549bb24c19e78147afdfb8ea5c866f399
GM-Benji <67319509+GM-Benji@users.noreply.github.com>
2023-12-19 00:27:38 +0100

    Update chess.c

--- chess.c
+++ chess.c
@@ -3,88 +3,33 @@
 #include "game.c"
 
 
-element* generate(char board[8][8],int color)
+
+move engine(set game,int color,int d)//d glebokosc, wywolywac od parzysstych 
 {
-	arrType moves=init();
-	move r={.pos1=0,.pos2=0};
-	element* head = malloc(sizeof(element));
-	*head = (element){ .ruch = r,.nastepny = NULL };
-	move ruch={.pos1=-1,.pos2=-1};
-	for (int i = 0; i < 8; i++)
+	int best=0;
+	element *head = malloc(sizeof(element));
+	head = generate(game,color);
+	while(head)
 	{
-		for (int j = 0; j < 8; j++)
-		{
-			if ((board[i][j] == 'n' && color==0) || (board[i][j] == 'N' && color==1))//skoczki
-			{
-				for (int k = 0; k < 8; k++)
-				{
-					ruch=(move){.pos1=i*10+j,.pos2=i*10+j+moves.arr[0][k]};
-					if(ifLegal(color,ruch,board))
-					{
-						utworz(ruch,head);
-					}
-				}
-			}
-			if ((board[i][j] == 'P' && color))//biale pionki
-			{
-				for (int k = 0; k < 4; k++)
-				{
-					ruch=(move){.pos1=i*10+j,.pos2=i*10+j+moves.arr[1][k]};
-					if(ifLegal(color,ruch,board))
-					{
-						utworz(ruch,head);
-					}
-				}
-			}
-			if ((board[i][j] == 'r' && color==0) || (board[i][j] == 'R' && color==1)) //wieze
-            {
-                for(int k=0; k < 32; k++)
-                {
-                    ruch=(move){.pos1=i*10+j,.pos2=i*10+j+moves.arr[3][k]};
-                    if(ifLegal(color,ruch,board))
-					{
-						utworz(ruch,head);
-					}
-                }
-            }
-            if ((board[i][j] == 'b' && color==0) || (board[i][j] == 'B' && color==1)) //wieze
-            {
-                for(int k=0; k < 32; k++)
-                {
-                    ruch=(move){.pos1=i*10+j,.pos2=i*10+j+moves.arr[4][k]};
-                    if(ifLegal(color,ruch,board))
-					{
-						utworz(ruch,head);
-					}
-                }
-            }
-            if ((board[i][j] == 'k' && color==0) || (board[i][j] == 'K' && color==1)) //wieze
-            {
-                for(int k=0; k < 8; k++)
-                {
-                    ruch=(move){.pos1=i*10+j,.pos2=i*10+j+moves.arr[5][k]};
-                    if(ifLegal(color,ruch,board))
-					{
-						utworz(ruch,head);
-					}
-                }
-            }
-		}
+		move temp=head->ruch;
+		char copyBoard[8][8];
+        strcpy(copyBoard,game.board);
+        copyBoard[digi(temp.pos2,0)][digi(temp.pos2,1)] = game.board[digi(temp.pos1,0)][digi(temp.pos1,1)]; // stadardowy ruch
+        copyBoard[digi(temp.pos1,0)][digi(temp.pos1,1)] = '#'; 
 	}
-	return head;
 }
 
 void main()
 {
 	//char board[8][8] = { "rnbqkbnr","pppppppp","########","########","########","########","PPPPPPPP","RNBQKBNR"};
-	char board[8][8] = { "##n#####","########","########","r######r","########","###w##n#","###P#k##","#K######"};
-	move x={.pos1=30,.pos2=0};
-	drawBoard(board);
+	char board[8][8] = { "##b#k###","########","########","#####Pp#","########","#######K","########","########"};
+	move x={.pos1=74,.pos2=72};
+	set game=setInit(board);
+	drawBoard(game.board);
+	//printf("%d\n",ifCastle(game.board,1,x));
+	//drawBoard(game.board);
 	element *head = malloc(sizeof(element));
-	head = generate(board,0);
-
+	head = generate(game,1);
 	showL(head->nastepny);
 	zniszcz(head);
-	//printf("%d\n",ifLegal(0,x,board));
-
 }
