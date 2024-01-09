77a2fab4eb7474e15776a6f635dddfa69d7544cc
GM-Benji <67319509+GM-Benji@users.noreply.github.com>
2024-01-09 11:56:05 +0100

    Update engine.c

--- engine.c
+++ engine.c
@@ -2,10 +2,11 @@ typedef struct bestReturn {
     move x;
     int value;
 }bestReturn;
-bestReturn engine(set game,int color,int d)//d glebokosc, wywolywac od parzysstych 
+bestReturn engine(set game,int color,int d)//d glebokosc
 {
 	bestReturn best;
 	bestReturn current;
+	current.value=0;
 	best.value=0;
 	element *head = malloc(sizeof(element));
 	head = generate(game,color);
@@ -30,22 +31,22 @@ bestReturn engine(set game,int color,int d)//d glebokosc, wywolywac od parzyssty
 	    {
 	        best.x.pos1=100;
 	        best.x.pos2=100;
+			best.value=0;
 	        return best;
 	    }
 	}
 	head=head->nastepny;
+	best.x=head->ruch;
+	//showL(head);
 	for(;head;head=head->nastepny)
 	{
+		
 		move temp=head->ruch;
-		char copyBoard[8][8];
-        strcpy(copyBoard,game.board);
-        copyBoard[digi(temp.pos2,0)][digi(temp.pos2,1)] = game.board[digi(temp.pos1,0)][digi(temp.pos1,1)]; // stadardowy ruch
-        copyBoard[digi(temp.pos1,0)][digi(temp.pos1,1)] = '#'; 
-        element *head2 = malloc(sizeof(element));
+        game.board[digi(temp.pos2,0)][digi(temp.pos2,1)] = game.board[digi(temp.pos1,0)][digi(temp.pos1,1)]; // stadardowy ruch
+        game.board[digi(temp.pos1,0)][digi(temp.pos1,1)] = '#'; 
+        element *head2 = malloc(sizeof(element));  
 	    head2 = generate(game,!color);
-	    for(;head2;head2=head2->nastepny)
-	    {
-	        if(!head->nastepny)
+		if(!head2->nastepny)
 	        {
 	            if(isCheck(game.board,!color))
 	            {
@@ -64,19 +65,52 @@ bestReturn engine(set game,int color,int d)//d glebokosc, wywolywac od parzyssty
 	            {
 	                best.x.pos1=100;
 	                best.x.pos2=100;
+					best.value=0;
 	                return best;
 	            }
 	        }
-	    if(d) current=engine(game,color,--d);
-        else current.value=evaluate(game.board);
-        if(current.value>best.value && color) best=current;
-        if(current.value<best.value && !color) best=current;
+		head2=head2->nastepny;
+		current.x=temp;
+	    for(;head2;head2=head2->nastepny)
+	    {
+			
+	        
+		move temp2=head2->ruch;
+		//printf("%d %d\n",temp.pos1,temp.pos2);
+		game.board[digi(temp2.pos2,0)][digi(temp2.pos2,1)] = game.board[digi(temp2.pos1,0)][digi(temp2.pos1,1)]; // stadardowy ruch
+        game.board[digi(temp2.pos1,0)][digi(temp2.pos1,1)] = '#'; 
+	    if(d)
+		{
+			bestReturn t=engine(game,color,--d);
+			if(current.value<t.value && !color)
+			{
+				current.value=t.value;
+			}
+			if(current.value>t.value && color)
+			{
+				current.value=t.value;
+			}
+		}
+        else 
+		{
+			int tempValue=evaluate(game.board);
+			if(current.value<tempValue && !color)
+			{
+				current.value=tempValue;
+			}
+			if(current.value>tempValue && color)
+			{
+				current.value=tempValue;
+			}
+		}
         
+	
 	   }
-        
-        zniszcz(head2);
+	   if(current.value>best.value && color) best=current;
+       if(current.value<best.value && !color) best=current;
+       zniszcz(&head2);
 	}
-	zniszcz(head);
+	zniszcz(&head);
 	
 	return best;
 }
